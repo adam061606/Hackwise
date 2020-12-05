d35f520ec88a673e594ad9309e830a9c4880e369
adam061606 <62824117+adam061606@users.noreply.github.com>
2020-12-05 10:57:02 +0800

    Create ViewController.swift

---
+++ Hackwise/Hackwise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hackwise
+//
+//  Created by Adam Tan on 5/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
